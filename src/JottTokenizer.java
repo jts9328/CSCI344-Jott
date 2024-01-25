bde2b4f8ef7e1f7b28e7c8b8f935ce205398065a
Ryan Symons <47405201+rsymons22@users.noreply.github.com>
2024-01-25 16:22:16 -0500

    Basic while loop for file reading and method stubs for dfa

--- src/JottTokenizer.java
+++ src/JottTokenizer.java
@@ -1,12 +1,18 @@
 package provided;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  * This class is responsible for tokenizing Jott code.
  * 
- * @author 
+ * @author Group 6
  **/
 
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File; 
+import java.io.FileNotFoundException;
 
 public class JottTokenizer {
 
@@ -16,7 +22,48 @@ public class JottTokenizer {
      * @param filename the name of the file to tokenize; can be relative or absolute path
      * @return an ArrayList of Jott Tokens
      */
-    public static ArrayList<Token> tokenize(String filename){
-		return null;
+    public static ArrayList<Token> tokenize(String filename) {
+		try {
+            File jottFile = new File(filename);
+            Scanner scanner = new Scanner(jottFile);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                char[] characters = data.split();
+
+                commentsBracketsSemicolon();
+
+                number();
+
+                operators();
+
+                idKeywordHeader();
+
+                string();                
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
 	}
+
+    public static void commentsBracketsSemicolon() {
+
+    }
+
+    public static void number() {
+
+    }
+
+    public static void operators() {
+
+    }
+
+    public static void idKeywordHeader() {
+
+    }
+
+    public static void string() {
+
+    }
 }
