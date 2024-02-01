91f97f095f01303eb086a9116e3e35bf76afd413
Gian Esteves <gae6065@rit.edu>
2024-02-01 15:53:40 -0500

    Merge operations

--- src/JottTokenizer.java
+++ src/JottTokenizer.java
@@ -13,16 +13,18 @@ import java.util.Scanner;
 
 public class JottTokenizer {
 
-	/**
+    /**
      * Takes in a filename and tokenizes that file into Tokens
      * based on the rules of the Jott Language
-     * @param filename the name of the file to tokenize; can be relative or absolute path
+     * 
+     * @param filename the name of the file to tokenize; can be relative or absolute
+     *                 path
      * @return an ArrayList of Jott Tokens
      */
     public static ArrayList<Token> tokenize(String filename) {
         ArrayList<Token> tokens = new ArrayList<>();
         int lineNum = 0;
-		try {
+        try {
             File jottFile = new File(filename);
             Scanner scanner = new Scanner(jottFile);
             while (scanner.hasNextLine()) {
@@ -30,9 +32,83 @@ public class JottTokenizer {
                 String data = scanner.nextLine();
                 char[] characters = data.toCharArray();
                 int index = 0;
-                while(index < characters.length) {
+                while (index < characters.length) {
                     // Giant if else
-                }             
+                    // Math operations
+                    if (characters[index] == '+') {
+                        tokens.add(new Token("+", filename, lineNum, TokenType.MATH_OP));
+                        index++;
+                    }
+                    if (characters[index] == '-') {
+                        tokens.add(new Token("-", filename, lineNum, TokenType.MATH_OP));
+                        index++;
+                    }
+                    if (characters[index] == '*') {
+                        tokens.add(new Token("*", filename, lineNum, TokenType.MATH_OP));
+                        index++;
+                    }
+                    if (characters[index] == '/') {
+                        tokens.add(new Token("/", filename, lineNum, TokenType.MATH_OP));
+                        index++;
+                    }
+                    // assign and == operation
+                    if (characters[index] == '=') {
+                        index++;
+                        if (index < characters.length) {
+                            if (characters[index] == '=') {
+                                tokens.add(new Token("==", filename, lineNum, TokenType.REL_OP));
+                                index++;
+                            } else {
+                                tokens.add(new Token("=", filename, lineNum, TokenType.ASSIGN));
+                            }
+                        } else {
+                            tokens.add(new Token("=", filename, lineNum, TokenType.ASSIGN));
+                        }
+                    }
+                    // Relational Operations <>
+                    if (characters[index] == '<') {
+                        index++;
+                        if (index < characters.length) {
+                            if (characters[index] == '=') {
+                                tokens.add(new Token("<=", filename, lineNum, TokenType.REL_OP));
+                                index++;
+                            } else {
+                                tokens.add(new Token("<", filename, lineNum, TokenType.REL_OP));
+                            }
+                        } else {
+                            tokens.add(new Token("<", filename, lineNum, TokenType.REL_OP));
+                        }
+                    }
+                    if (characters[index] == '>') {
+                        index++;
+                        if (index < characters.length) {
+                            if (characters[index] == '=') {
+                                tokens.add(new Token(">=", filename, lineNum, TokenType.REL_OP));
+                                index++;
+                            } else {
+                                tokens.add(new Token(">", filename, lineNum, TokenType.REL_OP));
+                            }
+                        } else {
+                            tokens.add(new Token(">", filename, lineNum, TokenType.REL_OP));
+                        }
+                    }
+                    // Relational Operation !=
+                    if (characters[index] == '!') {
+                        index++;
+                        if (index < characters.length) {
+                            if (characters[index] == '=') {
+                                tokens.add(new Token("!=", filename, lineNum, TokenType.REL_OP));
+                                index++;
+                            } else {
+                                System.out.println("Error: Expecting = after ! " + lineNum);
+                                break;
+                            }
+                        } else {
+                            System.out.println("Error: Expecting = after ! " + lineNum);
+                            break;
+                        }
+                    }
+                }
             }
             scanner.close();
         } catch (FileNotFoundException e) {
@@ -40,6 +116,6 @@ public class JottTokenizer {
             e.printStackTrace();
         }
         return tokens;
-	}
+    }
 
 }
