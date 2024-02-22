a7b08433afc097677399fea704f22c8b2ad82990
Dylan James Corpus <djc2250@rit.edu>
2024-02-22 16:40:53 -0500

    Implemented If, Else, ElseIf, While needs exception implementation

--- nodes/ElseNode.java
+++ nodes/ElseNode.java
@@ -3,6 +3,37 @@ package nodes;
 import provided.JottTree;
 
 public class ElseNode implements JottTree{
+    private BodyNode body;
+
+    // Else Statement Constructor
+    public ElseNode(BodyNode body) {
+        this.body = body;
+    }
+
+    // Else Null Constructor
+    public ElseNode() {
+        this.body = null;
+    }
+
+    public static ElseNode parseElse(ArrayList<Token> tokens) {
+        if (tokens == null || tokens.isEmpty()) {
+            // TODO Throw exception instead of NULL
+            return null; // No tokens to parse
+        }
+
+        Token token = tokens.get(0);
+
+        //check if there is an else statement
+        if (token.getToken().equal("Else")) {
+            token = tokens.remove(0); //remove else token
+            //parse body
+            BodyNode body = BodyNode.parseBody(tokens);
+            return ElseNode(body);
+        } else {
+            //no else return null else node
+            return ElseNode();
+        }
+    }
 
     @Override
     public String convertToJott() {
--- nodes/IfStmtNode.java
+++ nodes/IfStmtNode.java
@@ -3,6 +3,83 @@ package nodes;
 import provided.JottTree;
 
 public class IfStmtNode implements JottTree{
+    private ExprNode expr;
+    private BodyNode body;
+    private ArrayList<ElseIfNode> elseIfList;
+    private ElseNode else;
+
+    // If Statement Constructor
+    public IfStmtNode(ExprNode expr, BodyNode body, ArrayList<ElseIfNode> elseIfList, ElseNode else) {
+        this.expr = expr;
+        this.body = body;
+        this.elseIfList = elseIfList;
+        this.else = else;
+    }
+
+    public static IfStmtNode parseIfStmt(ArrayList<Token> tokens) {
+        if (tokens == null || tokens.isEmpty()) {
+            // TODO Throw exception instead of NULL
+            return null; // No tokens to parse
+        }
+
+        Token token = tokens.remove(0);
+
+        // Check if the first token is If
+        if (token.getToken().equals("If")) {
+            //Throw error if 'if' keyword is not found
+            System.err.println("Expected If, found: " + token.getToken());
+            // TODO Throw exception instead of NULL
+            return null;
+        }
+
+        //check for left bracket
+        token = token.remove(0);
+        if (token.getTokenType() != TokenType.L_BRACKET) {
+            System.err.println("Expected L_BRACKET, found: " + token.getTokenType())
+            // TODO Throw exception instead of NULL
+            return null;
+        }
+        //parse expression
+        ExprNode expr = ExprNode.parseExpr(tokens);
+        //check for right bracket
+        token = token.remove(0);
+        if (token.getTokenType() != TokenType.R_BRACKET) {
+            System.err.println("Expected R_BRACKET, found: " + token.getTokenType())
+            // TODO Throw exception instead of NULL
+            return null;
+        }
+
+        //check for left brace
+        token = token.remove(0);
+        if (token.getTokenType() != TokenType.L_BRACE) {
+            System.err.println("Expected L_BRACE, found: " + token.getTokenType())
+            // TODO Throw exception instead of NULL
+            return null;
+        }
+        //parse body
+        BodyNode body = BodyNode.parseBody(tokens);
+        //check for right brace
+        token = token.remove(0);
+        if (token.getTokenType() != TokenType.R_BRACE) {
+            System.err.println("Expected R_BRACE, found: " + token.getTokenType())
+            // TODO Throw exception instead of NULL
+            return null;
+        }
+
+        ArrayList<ElseIfNode> elseIfList;
+        token = tokens.get(0);
+        //check if there is an elseif next
+        while (token.getToken().equals("Elseif")) {
+            //apend to elseIfList
+            elseIfList.append(ElseIfNode.paseElseIf(tokens));
+            token = tokens.get(0);
+        }
+
+        //parse else
+        ElseNode else = ElseNode.parseElse(tokens);
+        
+        return new IfStmtNode(expr, body, elseIfList, else);
+    }
 
     @Override
     public String convertToJott() {
