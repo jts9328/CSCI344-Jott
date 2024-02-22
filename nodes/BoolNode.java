eb64e658ebbf445d283437933ea7cb7f4f16c7db
ehn4602 <ehn4602@g.rit.edu>
2024-02-22 14:47:53 -0500

    Implemented first version of BoolNode

--- nodes/BoolNode.java
+++ nodes/BoolNode.java
@@ -1,13 +1,46 @@
 package nodes;
 
 import provided.JottTree;
+import provided.Token;
+import provided.TokenType;
+
+import java.util.ArrayList;
 
 public class BoolNode implements JottTree{
+    private Token boolToken;
+
+    // Boolean Constructor
+    public BoolNode(Token value) {
+       this.boolToken = value;
+    }
+
+    public static BoolNode parseBoolNode(ArrayList<Token> tokens) {
+        Token boolToken = tokens.remove(0);
+
+        // Checks if the token type is ID_KEYWORD
+        if(boolToken.getTokenType() != TokenType.ID_KEYWORD){
+            return null; // Temp, Replace with exception
+        }
+        // IF keyword is "True" return valid BoolNode with token Value
+        else if(boolToken.getToken().equals("True")){
+            BoolNode node = new BoolNode(boolToken);
+            return node;
+        } 
+        // IF keyword is "False" return valid BoolNode with token Value
+        else if (boolToken.getToken().equals("False")){
+            BoolNode node = new BoolNode(boolToken);
+            return node;
+        } 
+        // Throw Exception since Token is not a valid Boolean Keyword
+        else {
+            return null; // Temp, Replace with exception
+        }
+
+    }
 
     @Override
     public String convertToJott() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+        return this.boolToken.getToken();
     }
 
     @Override
