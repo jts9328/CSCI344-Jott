eebbeda8cef930020d59304ae9620004fbeca0a5
jts9328 <jts9328@rit.edu>
2024-02-24 16:58:15 -0500

    implemented parse and covertToJott for FBodyNode

--- nodes/FBodyNode.java
+++ nodes/FBodyNode.java
@@ -1,13 +1,50 @@
 package nodes;
 
+import java.util.ArrayList;
+
+import exceptions.SyntaxErrorException;
+import provided.JottParser;
 import provided.JottTree;
+import provided.Token;
+import provided.TokenType;
 
 public class FBodyNode implements JottTree{
 
+    private BodyNode bodyNode;
+    private ArrayList<VarDecNode> varDecs = new ArrayList<VarDecNode>();
+
+    public FBodyNode(BodyNode bodyNode, ArrayList<VarDecNode> varDecs){
+        this.bodyNode = bodyNode;
+        this.varDecs = varDecs;
+    }
+
+    public static FBodyNode parseFBodyNode(ArrayList<Token> tokens) throws SyntaxErrorException {
+        // EOF
+        if (tokens == null || tokens.isEmpty()) {
+            throw new SyntaxErrorException("Unexpected EOF", JottParser.lastToken);
+        }
+        Token token = tokens.get(0);
+        ArrayList<VarDecNode> varDecs = new ArrayList<VarDecNode>();
+        // loop until its not a variable declaration
+        while((token.getTokenType() == TokenType.ID_KEYWORD && 
+            (token.getToken().equals("Integer") || token.getToken().equals("Double") || 
+            token.getToken().equals("String") || token.getToken().equals("Boolean")))){
+            varDecs.add(VarDecNode.parseVarDecNode(tokens));
+        }
+        // checks and exception handling should be caught by parseBodyNode
+        BodyNode bodyNode = BodyNode.parseBodyNode(tokens);
+        return new FBodyNode(bodyNode, varDecs);
+    }
+
     @Override
     public String convertToJott() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+        String jott = "";
+        // if no variable declarations, should break out for loop
+        for(VarDecNode varDec: this.varDecs){
+            jott = jott + varDec.convertToJott();
+        }
+        jott = jott + bodyNode.convertToJott();
+        return jott;
     }
 
     @Override
