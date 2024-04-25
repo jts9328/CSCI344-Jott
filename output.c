6be199b87b6de1bef5e99136526f748f91748752
djc2250 <djc2250@rit.edu>
2024-04-25 16:18:16 -0400

    edited read me

--- output.c
+++ output.c
@@ -2,11 +2,29 @@
 #include <string.h>
 #include <stdlib.h>
 
+void foo(int x){
+printf("%d\n", x);
+}
+void baz(){
+printf("%s\n", "Hello World");
+}
+char* bar(double x){
+if (x>5.1)
+{return "bar";
+}
+baz();
+return "foo";
+}
 int main(void){
-char *sewer;
-char *x;
-sewer = malloc(sizeof(char)*26);
-strcpy(sewer,"Underground Lizard Gang");
-x = strcat(sewer, ", Rats");
-printf("%s\n", x);
+double y;
+int x;
+x = 5;
+y = 1.0;
+while (x>0)
+{foo(x);
+printf("%s\n", bar(y));
+x = x-1;
+y = y+1.1;
+}
+baz();
 }
